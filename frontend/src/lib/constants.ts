c98ca075424254356abdeb823ed8da11bbd0398e
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2023-10-31 22:13:47 -0700

    chore: add logs

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,3 +1,6 @@
+console.log('window.location.host', window.location.host)
+console.log('window.location.host', window.location)
+
 export const API_BASE_URL = window.location.host.includes('localhost')
   ? 'http://localhost:3000/api/v1'
   : `${window.location.host}/api/v1`
