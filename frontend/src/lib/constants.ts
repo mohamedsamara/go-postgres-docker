7a086ada52e55fadf95fda33aba45a535d5c7a8d
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2023-10-31 22:03:05 -0700

    chore: update frontend api-url

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,1 +1,3 @@
-export const API_BASE_URL = import.meta.env.VITE_API_URL
+export const API_BASE_URL = window.location.host.includes('localhost')
+  ? 'http://localhost:3000/api/v1'
+  : `${window.location.host}/api/v1`
